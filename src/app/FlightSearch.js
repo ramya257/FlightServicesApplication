dc14014434cb4ac82869dc6318d9c054e975c433
ramya257 <educate1996@gmail.com>
2018-10-11 12:53:56 +0530

    modified search flights

--- src/app/FlightSearch.js
+++ src/app/FlightSearch.js
@@ -10,21 +10,19 @@ var FlightSearchPipe = (function () {
     function FlightSearchPipe() {
     }
     FlightSearchPipe.prototype.transform = function (items, searchName, searchStarting, searchDestination) {
-        if (searchName != null || searchStarting != null || searchDestination != null) {
-            if (items && items.length > 0) {
-                return items.filter(function (item) {
-                    if (searchName && item.flightName.toLowerCase().indexOf(searchName.toLowerCase()) === -1) {
-                        return false;
-                    }
-                    if (searchStarting && item.flightStarting.toLowerCase().indexOf(searchStarting.toLowerCase()) === -1) {
-                        return false;
-                    }
-                    if (searchDestination && item.flightDestination.toString().toLowerCase().indexOf(searchDestination.toLowerCase()) === -1) {
-                        return false;
-                    }
-                    return true;
-                });
-            }
+        if (items && items.length > 0) {
+            return items.filter(function (item) {
+                if (searchName && item.flightName.toLowerCase().indexOf(searchName.toLowerCase()) === -1) {
+                    return false;
+                }
+                else if (searchStarting && item.flightName.toLowerCase().indexOf(searchStarting.toLowerCase()) === -1) {
+                    return false;
+                }
+                else if (searchDestination && item.flightName.toLowerCase().indexOf(searchDestination.toLowerCase()) === -1) {
+                    return false;
+                }
+                return true;
+            });
         }
         else {
             return items;
--- src/app/FlightSearch.ts
+++ src/app/FlightSearch.ts
@@ -8,27 +8,25 @@ import { Flight } from "./Flight";
 export class FlightSearchPipe implements PipeTransform {
     transform(items: Flight[],searchName:string,searchStarting:string,searchDestination:string) {
        
-       if(searchName!=null ||searchStarting!=null||searchDestination!=null){
-           if(items&&items.length>0){
-               return items.filter(item =>{
-               if (searchName && item.flightName.toLowerCase().indexOf(searchName.toLowerCase()) === -1){
-                   return false;
-               }
-               if (searchStarting && item.flightStarting.toLowerCase().indexOf(searchStarting.toLowerCase()) === -1){
-                   return false;
-               }
-               if (searchDestination && item.flightDestination.toString().toLowerCase().indexOf(searchDestination.toLowerCase()) === -1){
-                   return false;
-               }
-              
-               return true;
-          })
-            
+        if(items&&items.length>0){
+            return items.filter(item =>{
+                if (searchName && item.flightName.toLowerCase().indexOf(searchName.toLowerCase()) === -1){
+                   
+                    return false;
+                }
+                else if (searchStarting && item.flightName.toLowerCase().indexOf(searchStarting.toLowerCase()) === -1){
+                    
+                    return false;
+                }
+                else if (searchDestination && item.flightName.toLowerCase().indexOf(searchDestination.toLowerCase()) === -1){
+                    
+                    return false;
+                }
+                return true;
+           })
 
-}
 }else{
     return items;
-
 }
-    }
+}
 }
